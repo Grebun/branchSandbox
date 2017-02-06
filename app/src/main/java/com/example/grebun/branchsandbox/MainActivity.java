c555cbfa3e176679a2e6be874407e696275bf957
dBranch <dimautkin44@gmail.com>
2017-02-07 00:45:16 +0300

    branchSandbox_ver.0.1

--- app/src/main/java/com/example/grebun/branchsandbox/MainActivity.java
+++ app/src/main/java/com/example/grebun/branchsandbox/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //проба пера
+        setContentView(R.layout.activity_main);
     }
 }
